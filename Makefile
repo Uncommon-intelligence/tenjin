296117f98ec7b4e1ccbe9983833a2f3aa9bc7c53
Tim Warren <hello@timwarren.dev>
2023-02-08 16:14:52 -0500

    Adds conversation to the API

--- Makefile
+++ Makefile
@@ -6,9 +6,11 @@ gradio:
 	poetry run python app.py
 
 api:
+	docker-compose up -d localstack 
 	poetry run uvicorn api:app --reload
 
 conversation:
+	docker-compose up -d localstack 
 	poetry run python conversation.py
 
 lint:
--- conversation.py
+++ conversation.py
@@ -1,10 +1,15 @@
 import os
+import uuid
 import toml
+import boto3
+import json
 import gradio as gr
-from langchain import OpenAI, LLMChain, PromptTemplate, Pinecone
+from langchain import OpenAI, LLMChain, PromptTemplate
 from langchain.agents import Tool, initialize_agent
-from langchain.chains.conversation.memory import ConversationalBufferWindowMemory, ConversationBufferMemory
+from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
 from langchain.utilities import GoogleSearchAPIWrapper
+
+BUCKET_NAME = "conversations"
 
 with open("config.toml") as f:
     config = toml.load(f)
@@ -14,6 +19,28 @@ os.environ["GOOGLE_API_KEY"] = config["google"]["api_key"]
 os.environ["GOOGLE_CSE_ID"] = config["google"]["cse_id"]
 
 search = GoogleSearchAPIWrapper()
+
+s3 = boto3.client("s3", endpoint_url="http://localhost:4566")
+
+
+def upload_json_to_s3(file_name: str, payload: dict):
+    s3.create_bucket(Bucket=BUCKET_NAME)
+    s3.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=json.dumps(payload))
+
+
+def fetch_conversation_data(file_name: str):
+    try:
+        response = s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
+        json_data = response["Body"].read().decode("utf-8")
+
+        conversation = json.loads(json_data)
+    except:
+        conversation = {}
+
+    history = conversation.get("history", [])
+    buffer = conversation.get("buffer", [])
+
+    return history, buffer
 
 tools = [
     Tool(
@@ -29,31 +56,15 @@ llm = OpenAI(
     model_name="text-davinci-003",
 )
 
-template = """
-You are an AI assistant that designed to help with general tasks.
 
-{history}
-Human: {user_input}
-AI: 
-"""
+def load_conversation_chain(conversation_id: str):
+    history, buffer = fetch_conversation_data(conversation_id)
 
-prompt = PromptTemplate(input_variables=["history", "user_input"], template=template)
+    memory = ConversationalBufferWindowMemory(
+        k=10, memory_key="chat_history", buffer=buffer
+    )
 
-chatgpt_chain = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    verbose=True,
-    memory=ConversationalBufferWindowMemory(k=2),
-)
-
-
-def chat(user_input, history, memory):
-    memory = memory or ConversationalBufferWindowMemory(k=10, memory_key="chat_history")
-    history = history or []
-    # memory = ConversationBufferMemory(memory_key="chat_history")
-
-
-    agent_chain = initialize_agent(
+    chain = initialize_agent(
         tools=tools,
         llm=llm,
         agent="conversational-react-description",
@@ -61,30 +72,41 @@ def chat(user_input, history, memory):
         memory=memory,
     )
 
-    # chain = LLMChain(
-    #     llm=OpenAI(temperature=0),
-    #     prompt=prompt,
-    #     verbose=True,
-    #     memory=memory,
-    # )
+    return history, chain
 
-    response = agent_chain(user_input)
+
+def chat(conversation_id, user_input):
+    history, chain = load_conversation_chain(conversation_id)
+    response = chain(user_input)
+
     history.append((response["input"], response["output"]))
 
-    return history, history, memory, ""
+    upload_json_to_s3(
+        file_name=conversation_id,
+        payload={
+            "buffer": chain.memory.buffer,
+            "history": history,
+        },
+    )
+
+    return history, ""
+
+def reset_chat():
+    return [], uuid.uuid4()
 
 
 with gr.Blocks() as demo:
     with gr.Tab("Chat"):
-        memory = gr.State()
-        history = gr.State()
+        conversation_id = gr.Textbox(lines=1, value=uuid.uuid4(), show_label=False)
         chatbot = gr.Chatbot()
         user_input = gr.Textbox(lines=1, placeholder="Question", show_label=False)
         user_input.submit(
             chat,
-            [user_input, history, memory],
-            [chatbot, history, memory, user_input],
+            [conversation_id, user_input],
+            [chatbot, user_input],
         )
+        button = gr.Button("Reset", label="Reset", show_label=False)
+        button.click(reset_chat, [], [chatbot, conversation_id])
 
 if __name__ == "__main__":
     demo.launch(enable_queue=True, debug=True)
