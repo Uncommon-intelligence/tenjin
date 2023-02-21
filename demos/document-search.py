2d805667c6e127c65c76233e35e449cc1ab3f6f0
Tim Warren <hello@timwarren.dev>
2023-02-21 15:44:33 -0500

    Combined document-search demo

--- demos/document-search.py
+++ demos/document-search.py
@@ -11,7 +11,11 @@ from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 
 from tenjin.readers.pdf_reader import PDFReader
-from tenjin.chat_templates.books import book_prompt
+from chat_templates.books import book_prompt
+
+from typing import List
+from langchain import PromptTemplate
+from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
 
 with open("config.toml") as f:
     config = toml.load(f)
@@ -26,6 +30,63 @@ pinecone_index_name = config["pinecone"]["index_name"]
 pinecone_environment = config["pinecone"]["environment"]
 
 pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+
+
+def book_prompt(buffer=List[str]):
+    """
+    Returns a contextualized prompt for the chatbot
+
+    Returns:
+        prompt: contextualized prompt
+        memory: memory to use for the chatbot
+    """
+    template = """
+    You are a chatbot having a converesation with a human about the contents of a book or paper.
+    You should respond to the human's question with a relevant sentence from the document as if you were the author of the document.
+    If there is more than one author, reply as if you were the author of the most relevant sentence.
+    Answers should be written in markdown format.
+
+    {context}
+
+    {chat_history}
+    Human: {question}
+    Chatbot:
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["chat_history", "question", "context"],
+        template=template,
+    )
+
+    memory = ConversationalBufferWindowMemory(
+        memory_key="chat_history",
+        input_key="question",
+        buffer=buffer,
+    )
+
+    return prompt, memory
+
+
+def sammy_default():
+    template = """
+    You're a slack bot named Sammy having a conversation with a person or group of people.
+    If a question is asked that include code exapmle or a code snippet, you should respond with the relevant code snippet wrapped in markdown code blocks.
+
+    Previous conversation history
+    {chat_history}
+
+    New Input: {input}
+    {agent_scratchpad}
+
+    Sammy:
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["input", "chat_history", "agent_scratchpad"],
+        template=template,
+    )
+
+    return prompt
 
 llm = OpenAI(
     openai_api_key=openai_api_key,
